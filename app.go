3ff215f96a5ffdba8a21199f3fc39023b1700e11
marcos28ha <marcos.h.a8@gmail.com>
2024-01-22 15:22:53 +0100

    sample2

--- app.go
+++ app.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/pebbe/zmq4"
+)
+
 
 func main() {
     fmt.Println("Miguelito Maravillas")
---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.21.6
+
+require github.com/pebbe/zmq4 v1.2.10 // indirect
